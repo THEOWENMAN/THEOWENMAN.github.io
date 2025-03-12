9e823f1ffc477655d1740f26a92d1ef9580d7115
THEOWENMAN <owentang799@gmail.com>
2025-03-12 13:43:51 -0600

    Terrain generation demo

--- index.md
+++ index.md
@@ -4,6 +4,7 @@
 - [Bouncing Balls](bouncing-balls)
 - [Generative Art](art)
 - [Perlin Noise Demo](perlin)
+- [Terrain Generation](terrain)
 
 # CS30 Projects
 - [Interactive Scene](interactive-scene)
---
+++ terrain/sketch.js
@@ -0,0 +1,46 @@
+// Terrain Generation Demo
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let terrain = [];
+const NUMBER_OF_RECTS = 2000;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  generateTerrain(width/NUMBER_OF_RECTS);
+  
+}
+
+function draw() {
+  background(220);
+  stroke("green");
+  fill("green");
+
+  for (let someRect of terrain){
+    rect(someRect.x,someRect.y, someRect.w, someRect.h);
+  }
+}
+
+function generateTerrain(widthOfRect){
+  let time = 0;
+  let deltaTime = 0.002;
+  for (let i = 0; i < NUMBER_OF_RECTS; i++){
+    let theHeight = noise(time) * height;
+    terrain.push(spawnRectangle(i * widthOfRect,theHeight,widthOfRect));
+    time += deltaTime;
+  }
+}
+
+function spawnRectangle(leftSide, rectHeight, rectWidth){
+  let theRect = {
+    x: leftSide,
+    y: height - rectHeight,
+    w: rectWidth,
+    h: rectHeight,
+  };
+  return theRect;
+}
