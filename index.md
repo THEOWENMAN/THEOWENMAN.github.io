f30d9cf9f9dc61ffdba8d280b649688193858a21
THEOWENMAN <owentang799@gmail.com>
2025-03-25 14:15:02 -0600

    Created game of life demo

--- index.md
+++ index.md
@@ -9,6 +9,7 @@
 - [2D Array Grid](grid)
 - [Grid Neighbours](neighbours)
 - [Rectangle Grid](rectangle-grid)
+- [Game of Life](game-of-life)
 
 # CS30 Projects
 - [Interactive Scene](interactive-scene)
--- rectangle-grid/sketch.js
+++ rectangle-grid/sketch.js
@@ -1,65 +1,51 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// 2D Array Rectangle Grid Demo
+// Pick a cell size, then fill the screen with as many as possible.
+// This will likely be rectangular instead of square...
 
 const CELL_SIZE = 50;
 let grid;
 let rows;
 let cols;
 
-
-
-
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cols = Math.ceil(width/CELL_SIZE);
   rows = Math.ceil(height/CELL_SIZE);
-  grid = generateRandomGrid(cols,rows);
-
+  grid = generateRandomGrid(cols, rows);
 }
-
 
 function draw() {
   background(220);
   displayGrid();
 }
 
-
-
-
-function generateRandomGrid(cols,rows){
-  let newGrid = [];
-  for (let y = 0; y < rows; y++ ){
-    newGrid.push([]);
-    for(let x = 0; x < cols; x++){
-      // toss a zero or one randomly
-      if (random(0,100) < 50){
-        newGrid[y].push(0);
+function displayGrid() {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
       }
-      else{
-        newGrid[y].push[1];
+      else if (grid[y][x] === 1) {
+        fill("black");
       }
-      return newGrid;
-    } 
+      square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
+    }
   }
 }
 
-
-
-function displayGrid(){
-  for (let y = 0; y < rows; y++){
-    for(let x = 0; x < cols; x++){
-      if (grid[y][x] === 0){
-        fill("white");
+function generateRandomGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+      //toss a 0 or 1 in randomly
+      if (random(100) < 50) {
+        newGrid[y].push(0);
       }
-      else if(grid[y][x] === 1){
-        fill("height");
+      else {
+        newGrid[y].push(1);
       }
-      rect(x*CELL_SIZE,y*CELL_SIZE, CELL_SIZE, CELL_SIZE);
     }
   }
+  return newGrid;
 }
